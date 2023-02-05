118571a125a1a1e9e43bbd41b87b4ca06d363e9e
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-02-05 20:30:00 +0100

    Rule class, again
    Now it has the missing evaluate method. I've asked the reason and
    ChatGPT answered that it is definitely needed

--- lib/ruleur/rule.rb
+++ lib/ruleur/rule.rb
@@ -3,11 +3,15 @@
 module Ruleur
   # The Rule class represents a business rule in the BRMS.
   class Rule
-    attr_reader :conditions, :actions
+    attr_reader :condition, :action
 
-    def initialize(conditions, actions)
-      @conditions = conditions
-      @actions = actions
+    def initialize(condition, action)
+      @condition = condition
+      @action = action
+    end
+
+    def evaluate(facts)
+      facts.select { |fact| condition.call(fact) }.each { |fact| action.call(fact) }
     end
   end
 end
--- spec/ruleur/rule_spec.rb
+++ spec/ruleur/rule_spec.rb
@@ -4,13 +4,28 @@ require 'spec_helper'
 
 RSpec.describe Ruleur::Rule do
   describe '#initialize' do
-    it 'accepts conditions and actions as arguments' do
-      conditions = [double, double]
-      actions = [double, double]
-      rule = described_class.new(conditions, actions)
+    it 'takes a condition and an action as arguments' do
+      condition = proc {}
+      action = proc {}
 
-      expect(rule.conditions).to eq(conditions)
-      expect(rule.actions).to eq(actions)
+      rule = described_class.new(condition, action)
+
+      expect(rule.condition).to eq(condition)
+      expect(rule.action).to eq(action)
+    end
+  end
+
+  describe '#evaluate' do
+    let(:condition) { proc { |fact| fact > 5 } }
+    let(:action) { proc { |fact| results << fact } }
+    let(:rule) { described_class.new(condition, action) }
+    let(:results) { [] }
+    let(:facts) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
+
+    it 'evaluates the rule against a set of facts' do
+      rule.evaluate(facts)
+
+      expect(results).to eq([6, 7, 8, 9, 10])
     end
   end
 end
