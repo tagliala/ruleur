00932361cf9a123e826645928de381fd724160c4
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-02-06 23:35:03 +0100

    Update fact and working memory

--- lib/ruleur/fact.rb
+++ lib/ruleur/fact.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 module Ruleur
-  # The Fact class represents a piece of information that is processed by the Rete algorithm.
-  # It is a PORO (Plain Old Ruby Object) that holds the data for the rules to operate on.
+  # The Fact class represents a piece of information processed by the Rete algorithm in the BRMS.
+  # This class acts as a data container, holding the relevant information for the rules to operate on.
+  # Instead of using the original data source directly, the information is extracted and stored as instances of the Fact class.
+  # This allows for better management and processing of the data within the BRMS.
   class Fact
     attr_reader :attributes
 
